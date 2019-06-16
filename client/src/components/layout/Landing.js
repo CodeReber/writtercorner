a910ce5d8d903ca647ac1b137ff0cc757c8439cd
codereber <jamesmpyle@gmail.com>
2019-06-16 09:38:44 -0400

    update

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -12,17 +12,16 @@ const Landing = ({ isAuthenticated }) => {
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
+          <h1 className='x-large'>Writter's Corner</h1>
           <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            Share your ideas with others
           </p>
           <div className='buttons'>
             <Link to='/register' className='btn btn-primary'>
-              Sign Up
+             Register
             </Link>
             <Link to='/login' className='btn btn-light'>
-              Login
+             Sign In 
             </Link>
           </div>
         </div>
