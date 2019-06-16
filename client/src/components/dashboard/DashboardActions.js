f0d582eb5134cbbd11b67ce3c9598966725a9920
codereber <jamesmpyle@gmail.com>
2019-06-16 12:34:15 -0400

    update

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -7,12 +7,12 @@ const DashboardActions = () => {
       <Link to='/edit-profile' class='btn btn-light'>
         <i className='fas fa-user-circle text-primary' /> Edit Profile
       </Link>
-      <Link to='/add-experience' class='btn btn-light'>
+      {/* <Link to='/add-experience' class='btn btn-light'>
         <i className='fab fa-black-tie text-primary' /> Add Experience
       </Link>
       <Link to='/add-education' class='btn btn-light'>
         <i className='fas fa-graduation-cap text-primary' /> Add Education
-      </Link>
+      </Link> */}
     </div>
   );
 };
