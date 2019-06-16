3d2c0fccd6e8a7912619da10a6a8e07df55dc740
codereber <jamesmpyle@gmail.com>
2019-06-16 08:46:36 -0400

    updatew

---
+++ client/build/precache-manifest.c36ec734c9107f35a1a99238d20258fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c412c3d4056267823b644410632bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c180f667b8fe76ed43e",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "8c180f667b8fe76ed43e",
+    "url": "/static/js/main.eaafe610.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
